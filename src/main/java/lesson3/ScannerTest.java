64de5e974ee59c50ee3bd959709a0d661cf2ddae
Andrey Pomelov <k-udm@ya.ru>
2023-05-04 23:02:09 +0300

    Lesson 3

---
+++ src/main/java/lesson3/ScannerTest.java
@@ -0,0 +1,33 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class ScannerTest {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name = scanner.nextLine();
+//        System.out.println("Вы ввели: " + word);
+        // Комментарий
+
+        System.out.println("Введите пол");
+        char gender = scanner.nextLine().charAt(0);
+
+        System.out.println("Введите возраст");
+        int age = scanner.nextInt();
+
+        System.out.println("Введите номер телефона");
+        long phoneNumber = scanner.nextLong();
+
+        System.out.println("Введите идентификатор");
+        double id = scanner.nextDouble();
+
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("ID: " + id);
+    }
+}
---
+++ src/main/java/lesson3/TypeTest.java
@@ -0,0 +1,55 @@
+package lesson3;
+
+public class TypeTest {
+
+    public static void main(String[] args) {
+
+        byte b = 45;
+        short sh;
+        sh = b;
+
+        System.out.println(sh);
+
+        sh = 1000;
+        b = (byte) sh;
+
+        System.out.println(b);
+
+        int i = 100;
+        double d = 55.55;
+        System.out.println(i + d);
+        int i1 = 40;
+
+        System.out.println(i / (double) i1);
+
+        char ch = '9';
+
+        String str = "9";
+
+        int a = ch - '0';
+        System.out.println(a);
+
+        int c = Integer.parseInt(String.valueOf(ch));
+        System.out.println(c);
+
+        int e = Character.getNumericValue(ch);
+        System.out.println(e);
+
+        int f = Integer.parseInt(str);
+        System.out.println(f);
+
+        String word = "Hello!";
+        System.out.println(word.length());
+        System.out.println(word.charAt(4));
+        System.out.println(word.substring(2, 4));
+
+        System.out.println(word.contains("l"));
+        System.out.println(word.contains("w"));
+
+        System.out.println(word.toUpperCase());
+        System.out.println(word.toLowerCase());
+
+        System.out.println(word.replace('e', 'u'));
+        System.out.println(word.startsWith("He"));
+    }
+}
